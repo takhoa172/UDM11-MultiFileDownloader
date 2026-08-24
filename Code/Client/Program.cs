cae529691c1b0213e4b0fcdb48ce1189766cec1a
takhoa172 <trinhanhkhoa172@gmail.com>
2026-08-24 10:02:13 +0700

    Revert "feat: add authentication protocol"

--- Code/Client/Program.cs
+++ Code/Client/Program.cs
@@ -1,9 +1,1 @@
-using Shared;
-
-Console.WriteLine("Nhap password:");
-string password = Console.ReadLine() ?? "";
-
-string hash = PasswordHasher.HashPassword(password);
-
-Console.WriteLine("SHA-256:");
-Console.WriteLine(hash);
+﻿Console.WriteLine("Hello, World!");
